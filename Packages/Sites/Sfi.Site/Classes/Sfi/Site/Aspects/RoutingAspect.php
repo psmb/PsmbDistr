facaafdf61799dc786057e128c27f6e5bddcddc8
Dmitri Pisarev <dimaip@gmail.com>
2016-09-05 02:57:22 +0300

    Pimp router to use ES index for performance

--- Packages/Sites/Sfi.Site/Classes/Sfi/Site/Aspects/RoutingAspect.php
+++ Packages/Sites/Sfi.Site/Classes/Sfi/Site/Aspects/RoutingAspect.php
@@ -15,6 +15,12 @@ class RoutingAspect {
 
 	/**
 	 * @Flow\Inject
+	 * @var \Flowpack\ElasticSearch\ContentRepositoryAdaptor\ElasticSearchClient
+	 */
+	protected $elasticSearchClient;
+
+	/**
+	 * @Flow\Inject
 	 * @var NodeTypeManager
 	 */
 	protected $nodeTypeManager;
@@ -24,6 +30,36 @@ class RoutingAspect {
 	 * @var NodeSearchServiceInterface
 	 */
 	protected $nodeSearchService;
+
+
+	/**
+	 * @Flow\Inject
+	 * @var \Flowpack\ElasticSearch\ContentRepositoryAdaptor\LoggerInterface
+	 */
+	protected $logger;
+
+	/**
+	 * Get the ElasticSearch request
+	 * @var string
+	 * @return array
+	 */
+	protected function getRequest($pathSegment) {
+		return array(
+			'query' => array(
+				'bool' => array(
+					'must' => array(
+						array(
+							'term' => array('uriPathSegment' => $pathSegment)
+						),
+						array(
+							'term' => array('__parentPath' => "/sites/site/a")
+						)
+					)
+				)
+			),
+			'fields' => array('__path')
+		);
+	}
 
 	/**
 	 * Hardcoded performace optimization for /a/*.html links
@@ -37,27 +73,36 @@ class RoutingAspect {
 		$relativeRequestPath = $joinPoint->getMethodArgument('relativeRequestPath');
 
 		// If within /a/path
-		if (strlen($relativeRequestPath) > 3 && substr($relativeRequestPath, 0, strlen('a/')) === 'a/') {
-			/** @var NodeInterface $siteNode */
-			$siteNode = $joinPoint->getMethodArgument('siteNode');
-			$documentNodeType = $this->nodeTypeManager->getNodeType('TYPO3.Neos:Document');
-			$context = $siteNode->getContext();
-
-			$baseNode = $siteNode->getNode('a');
+		if (substr($relativeRequestPath, 0, strlen('a/')) === 'a/') {
 			$pathSegments = explode('/', $relativeRequestPath);
 			$pathSegment = end($pathSegments);
-			$nodes = $this->nodeSearchService->findByProperties(['uriPathSegment' => $pathSegment], [$documentNodeType], $context, $baseNode);
-			$filteredNodes = array_filter($nodes, function ($currentNode) use ($baseNode) {
-				// Only consider direct children
-				return $currentNode->getParent()->getIdentifier() === $baseNode->getIdentifier();
-			});
-			if ($filteredNodes) {
-				$node = reset($filteredNodes);
-				$nodeName = $node->getName();
+
+			$response = $this->elasticSearchClient->getIndex()->request('GET', '/_search', array(), json_encode($this->getRequest($pathSegment)));
+			$result = $response->getTreatedContent();
+			if (array_key_exists('hits', $result) && is_array($result['hits']) && $result['hits']['total'] > 0) {
+				$hit = reset($result['hits']['hits']);
+				return reset($hit['fields']['__path']);
 			} else {
-				return false;
+				// Fallback to the search service, if not results from ES. Needed, because the newly created node may not yet be in the index
+				/** @var NodeInterface $siteNode */
+				$siteNode = $joinPoint->getMethodArgument('siteNode');
+				$documentNodeType = $this->nodeTypeManager->getNodeType('TYPO3.Neos:Document');
+				$context = $siteNode->getContext();
+
+				$baseNode = $siteNode->getNode('a');
+				$nodes = $this->nodeSearchService->findByProperties(['uriPathSegment' => $pathSegment], [$documentNodeType], $context, $baseNode);
+				$filteredNodes = array_filter($nodes, function ($currentNode) use ($baseNode) {
+					// Only consider direct children
+					return $currentNode->getParent()->getIdentifier() === $baseNode->getIdentifier();
+				});
+				if ($filteredNodes) {
+					$node = reset($filteredNodes);
+					$nodeName = $node->getName();
+				} else {
+					return false;
+				}
+				return 'a/' . $nodeName;
 			}
-			return 'a/' . $nodeName;
 		} else {
 			return $joinPoint->getAdviceChain()->proceed($joinPoint);
 		}
