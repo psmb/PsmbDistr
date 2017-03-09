8c3ddc1992616d99c3915ce43961f75a6e81031a
Dmitri Pisarev <dimaip@gmail.com>
2017-03-09 12:06:12 +0300

    TASK: pin themes and fix translation

--- Packages/Sites/Sfi.Site/Frontend/js/main.js
+++ Packages/Sites/Sfi.Site/Frontend/js/main.js
@@ -178,9 +178,7 @@ $(function() {
 
 //Shuffle move to separate file
 (function($){
-
 	$.fn.shuffle = function() {
-
 		var allElems = this.get(),
 			getRandom = function(max) {
 				return Math.floor(Math.random() * max);
@@ -190,21 +188,28 @@ $(function() {
 					randEl = $(allElems[random]).clone(true)[0];
 				allElems.splice(random, 1);
 				return randEl;
-			 });
+			});
 
 		this.each(function(i){
 			$(this).replaceWith($(shuffled[i]));
 		});
 
-		return $(shuffled);
+		// Bring pinned to top
+		$(shuffled).each(function(i){
+			if ($(this).hasClass('js-interesting-themes__item--pinned')) {
+				shuffled.splice(i, 1);
+				$(shuffled).first().before($(this))
+			}
+		});
 
+		return $(shuffled);
 	};
 
 })(jQuery);
 
 
 $(function() {
-	// $(".js-interesting-themes li").shuffle();
+	$(".js-interesting-themes li").shuffle();
 	$(".js-interesting-themes ul").hideMaxListItems({
 		'max':6,
 		'moreText':'Еще',
