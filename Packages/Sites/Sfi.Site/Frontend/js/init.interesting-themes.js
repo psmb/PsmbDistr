17cd2c08a38afa5136e0890ab1208fb1cdb1536f
Dmitri Pisarev <dimaip@gmail.com>
2021-03-01 17:11:42 +0300

    Stop shuffling themes

--- Packages/Sites/Sfi.Site/Frontend/js/init.interesting-themes.js
+++ Packages/Sites/Sfi.Site/Frontend/js/init.interesting-themes.js
@@ -1,9 +1,9 @@
-$(function() {
-	$(".js-interesting-themes li").shuffle();
-	$(".js-interesting-themes ul").hideMaxListItems({
-		'max':6,
-		'moreText':'Еще',
-		'lessText':'Свернуть',
-		'moreHTML': '<p class="maxlist-more"><a href="#">Другие темы</a></p>'
-	});
+$(function () {
+    // $(".js-interesting-themes li").shuffle();
+    $(".js-interesting-themes ul").hideMaxListItems({
+        max: 6,
+        moreText: "Еще",
+        lessText: "Свернуть",
+        moreHTML: '<p class="maxlist-more"><a href="#">Другие темы</a></p>',
+    });
 });
